92c5c2ffbe977016c443c86be71fe9f12b23680f
nicoquartero <nicoquartero@gmail.com>
2021-08-27 20:47:00 -0300

    Agrega suma en PHP y muestra resultados

---
+++ operaciones.php
@@ -0,0 +1,18 @@
+<?php
+$n1 = $_GET['n1'];
+$n2 = $_GET['n2'];
+
+$suma = $n1 + $n2
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Operaciones</title>
+	<title></title>
+</head>
+<body>
+	<h1>Resultado</h1>
+	<?php echo "<p>La suma $n1 + $n2 = $suma</p>";?>
+</body>
+</html>
