ac3946dfb088977474e8416ac8d14a086922dee3
nicoquartero <nicoquartero@gmail.com>
2021-08-27 20:56:24 -0300

    Agrega el producto

--- operaciones.php
+++ operaciones.php
@@ -2,17 +2,19 @@
 $n1 = $_GET['n1'];
 $n2 = $_GET['n2'];
 
-$suma = $n1 + $n2
+$suma = $n1 + $n2;
+$producto = $n1 * $n2;
 ?><!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
 	<title>Operaciones</title>
-	<title></title>
+	<title>Resultado</title>
 </head>
 <body>
 	<h1>Resultado</h1>
 	<?php echo "<p>La suma $n1 + $n2 = $suma</p>";?>
+	<?php echo "<p>El producto $n1 + $n2 = $producto</p>";?>
 </body>
 </html>
