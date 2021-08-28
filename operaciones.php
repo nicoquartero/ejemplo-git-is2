1c4bf7aa18e7e937f981cc8346feb7e0fcd8ddf9
nicoquartero <nicoquartero@gmail.com>
2021-08-27 21:04:37 -0300

    Agrega la resta a operaciones.php
    git log
    q
    q
    
    quit
    return
    RETURN
    Q
    q
    git status

--- operaciones.php
+++ operaciones.php
@@ -4,6 +4,7 @@ $n2 = $_GET['n2'];
 
 $suma = $n1 + $n2;
 $producto = $n1 * $n2;
+$resta = $n1 - $n2;
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -16,5 +17,6 @@ $producto = $n1 * $n2;
 	<h1>Resultado</h1>
 	<?php echo "<p>La suma $n1 + $n2 = $suma</p>";?>
 	<?php echo "<p>El producto $n1 + $n2 = $producto</p>";?>
+	<?php echo "<p>La resta $n1 + $n2 = $resta</p>";?>
 </body>
 </html>
